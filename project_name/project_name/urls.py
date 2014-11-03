9d1e980f88a19e18bbe70a26cc0a44055e7b81c5
PRICGREY <privgrey@gmail.com>
2014-11-03 11:47:36 +0800

    add media local serving

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,3 +11,13 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+# enables local media serving
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+)
+
